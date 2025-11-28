05ae10de11fdf0b8b554159f4048f79cbba0ab6d
zorales <clostuuu@gmail.com>
2025-11-28 17:06:06 +0300

    Практика с if else

---
+++ course-1/practices/Consolka/Consolka/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 задание
+            //int a = 12;
+            //int b = 5;
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a / b);
+            //Console.WriteLine(a % b);
+
+            //2 задание
+            //string a = Console.ReadLine();
+            //Console.WriteLine($"Привет " + a);
+
+            //3 задание
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int n = 6;
+            //Console.WriteLine($"Сумма: {n}" + (a + b));
+
+            //4 Задание
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Площадь треугольника: " + (a * b));
+
+            //5 Задание 
+            //int number = 0;
+
+            //if (number > 0) Console.WriteLine("Число положительное!");
+            //if (number < 0) Console.WriteLine("Число отрицательное!");
+            //if (number == 0) Console.WriteLine("Число равно нулю!");
+
+            //6 Задание
+
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //if (age >= 18) Console.WriteLine("Совершеннолетний!");
+            //else Console.WriteLine("Несовершеннолетний!");
+
+            //7 Задание
+
+            //int num = 7;
+            //if (num % 2 == 0) Console.WriteLine("Число четное!");
+            //else Console.WriteLine("Число нечетное!");
+
+            //8 Задание
+
+            //int a = -5;
+            //int b = -7;
+            //if (a > 0 && b > 0) Console.WriteLine("Оба положительные!");
+            //if (a > 0 || b > 0) Console.WriteLine("Одно положительное!");
+            //if (a < 0) Console.WriteLine("А не положительное!");
+
+            //9 Задание
+            //int grade = Convert.ToInt32(Console.ReadLine());
+
+            //if (grade < 3) Console.WriteLine("Неудовлетворительно!");
+            //if (grade == 3) Console.WriteLine("Удовлетворительно!");
+            //if (grade == 4) Console.WriteLine("Хорошо!");
+            //if (grade == 5) Console.WriteLine("Отлично!");
+        }
+    }
+}
