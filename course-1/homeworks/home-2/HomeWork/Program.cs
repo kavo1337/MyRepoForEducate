bc07a5d3663783e9003028e574b451962a2c41a2
zorales <clostuuu@gmail.com>
2025-12-01 23:36:08 +0300

    home-2

---
+++ course-1/homeworks/home-2/HomeWork/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1 задание
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Convert: " + (a * 9 / 5 + 32));
+
+            //2 задание
+            //int a = Convert.ToInt32(Console.ReadLine());int b = Convert.ToInt32(Console.ReadLine());int c = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Total: " + ((a + b + c) / 3));
+
+
+            //3 задание
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Сумма: " + (a + b));
+            //Console.WriteLine("Разность: " + (a - b));
+            //Console.WriteLine("Произведение: " + (a * b));
+            //Console.WriteLine("Частное: " + (a / b));
+
+        }
+    }
+}
