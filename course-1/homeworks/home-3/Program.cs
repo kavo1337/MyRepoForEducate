ed0842b6745cd93625b3306477d1b9904af88077
zorales <clostuuu@gmail.com>
2025-12-02 18:20:05 +0300

    home-3

---
+++ course-1/homeworks/home-3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 задание
+            //int[] marks = { 5, 5, 3, 2, 4, 5, 3 };
+            //double ball = 0;
+            //int five = 0;
+            //int two = 0;
+            //foreach (int num in marks)
+            //{
+            //    ball = ball + num;
+            //    if (num == 5) { five = five + 1; }
+            //    if (num == 2) { two = two + 1; }
+
+            //}
+            //Console.WriteLine("Средний балл: " + Math.Round(ball / marks.Length, 2) + "\n"
+            //    + "Количество пятерок: " + five + "\n"
+            //    + "Количество двоек: " + two);
+
+            //2 задание
+            //int[] numbers = { 5, 5, 3, 2, 4, };
+            //Console.WriteLine("Не реверснутый: " + string.Join(", ", numbers) + "\n"
+            //    + "Реверснутый: " + string.Join(", ", numbers.Reverse()));
+
+            //3 задание
+            //for (int i = 1; i <= 20; i++) { 
+            //    if (i % 2 == 0) Console.WriteLine(i);
+            //    continue;
+            //}
+
+            //4 задание
+            //int attempt = 3;
+            //do {
+            //    Console.WriteLine($"У вас {attempt} попытки!!");
+            //    string password = Console.ReadLine(); 
+                
+            //    if (password == "1234")
+            //    {
+            //        Console.WriteLine("Доступ разрещен!");
+            //        break;
+            //    }else
+            //    {
+            //        attempt--;
+            //        Console.WriteLine("Доступ запрещен!");
+            //    }
+
+            //}
+            //while (attempt >= 1);
+        }
+    }
+}
