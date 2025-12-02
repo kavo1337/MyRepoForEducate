e5e1d2e48005f766f4470d6d3b3f82a511ba49bc
zorales <clostuuu@gmail.com>
2025-12-02 17:37:58 +0300

    Practica masivi

---
+++ course-1/practices/Masivi/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Masasivi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 задание 
+            //int[] numbers = new int[5];
+            //for (int a = 0; a < numbers.Length; a++)
+            //{
+            //    numbers[a] = a + 1;
+            //}
+            //Console.WriteLine(string.Join(", ", numbers));
+
+
+            // 2 задание 
+            //int[] marks = { 1, 2, 3, 4, 5 };
+            //int sum = 0;
+            //foreach (int i in marks)
+            //{
+            //    sum = sum + i;
+            //}
+            //Console.WriteLine("Total: " + (sum / marks.Length));
+
+            // 3 задание 
+            //var rand = new Random();
+            //int[] numbers = new int[8];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = rand.Next(1, 100);
+            //}
+
+            //Console.WriteLine("Max number: " + numbers.Max());
+
+            // 4 задание 
+            //string[] names = { "Banana", "Strawberry", "Apple", "Orange", "Mango" };
+            //foreach (string name in names) {
+            //    Console.WriteLine(name);
+            //}
+
+            // 5 задание 
+            //int[,] arr = {
+            //    { 1, 2, 3 },
+            //    { 4, 5, 6 },
+            //    { 7, 8, 9 },
+            //};
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr.GetLength(1); j++)
+            //    {
+            //        Console.Write(arr[i, j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // 6 задание 
+            //bool activated = false;
+            //do
+            //{
+            //    string input = Console.ReadLine();
+            //    if (input == "1234")
+            //    {
+            //        activated = true;
+            //        Console.WriteLine("Activated");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Incorrect password!");
+            //    }
+            //}
+            //while (!activated);
+        }
+    }
+}
---
+++ course-1/practices/Masivi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Masasivi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Masasivi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("41b2981b-6f6d-4bbc-91e4-dcffa9ad3f81")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
